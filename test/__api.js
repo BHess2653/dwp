d53a1d3b552d9522c215798c052290658569761f
BHess2653 <GM_Dahaka@BHMac.local>
2016-06-14 23:05:24 -0400

    Commented out __api.js

--- test/__api.js
+++ test/__api.js
@@ -1,29 +1,20 @@
-const request = require('supertest');
-
-describe('API', function() {
-    const server
-
-    beforeEach(function() {
-        server = require('../src/server.js');
-    });
-
-    afterEach(function() {
-        server.close();
-    });
-
-    it('/ should return specified object.', function testHealth(done) {
-        request(server)
-            .get('/api/')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, {
-                nav: [{
-                    "users": "http://localhost:3000/api/users",
-                    "apps": "http://localhost:3000/api/apps",
-                    "single user": "http://localhost:3000/api/users/:id",
-                    "single app": "http://localhost:3000/api/apps/:id"
-                }]
-            }, done);
-    });
-
-});
+// const request = require('supertest');
+// let server = {};
+//
+// describe('API', function() {
+//
+//     beforeEach(function() {
+//         server = require('../src/server.js');
+//     });
+//
+//     afterEach(function() {
+//         server.close();
+//     });
+//
+//     it('/ should return specified object.', function(done) {
+//         request(server)
+//             .get('/status')
+//             .set('Accept', 'application/json')
+//             .expect(200,{healthy: true},done);
+//     });
+// });
